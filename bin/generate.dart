5d68e5c5100c59fd10d6adcafa1a247efb156ff6
bred <default51rus@gmail.com>
2020-10-02 17:14:20 +0300

    trying to prevent double backslash

--- bin/generate.dart
+++ bin/generate.dart
@@ -335,7 +335,7 @@ class CodegenLoader extends AssetLoader{
   List listLangs = csvParser.getLanguages();
   for (String localeName in listLangs) {
     listLocales.add('"$localeName": $localeName');
-    String mapString = JsonEncoder.withIndent("    ").convert(csvParser.getLanguageMap(localeName));
+    String mapString = JsonEncoder.withIndent("    ").convert(csvParser.getLanguageMap(localeName)).replaceAll("\\n", "\n");
 
     classBuilder.writeln('  static const Map<String,dynamic> $localeName = <String, dynamic>${mapString};\n');
   }
